a2e12c6b72d94c024b387f0b95a00fbe7714ee76
Thallyson-Luiz <luizthallyson01@gmail.com>
2025-06-08 18:14:04 -0300

    implementação do verificador de CPF com validação de formato e cálculo de dígitos

---
+++ Exercicios/verificador_cpf.py
@@ -0,0 +1,111 @@
+import os
+vermelho = "\033[31m"
+verde = "\033[32m"
+branco = "\033[0m"
+
+while True:
+    cpf = input('Digite seu CPF: ')
+    cpf = cpf.replace(' ', '')
+    try:
+        if len(cpf) != 11:
+            raise ValueError
+        cpf = int(cpf)
+    except:
+        try:
+            primeiros_numeros_cpf, ultimosNumeros_cpf = cpf.split('-')
+            primeiros_numeros_cpf = primeiros_numeros_cpf.split('.')
+            primeiros_numeros_CPF = []
+            ultimosNumerosCPF = []
+            for numeros in primeiros_numeros_cpf:
+                if len(numeros) == 3:
+                    numeros = list(numeros)
+                    for n in numeros:
+                        if n.isnumeric():
+                            n = int(n)
+                            primeiros_numeros_CPF.append(n)
+                else:
+                    raise ValueError
+            
+            if len(ultimosNumeros_cpf) == 2:
+                ultimosNumeros_cpf = list(ultimosNumeros_cpf)
+                for numeros in ultimosNumeros_cpf:
+                    if numeros.isnumeric():
+                        numeros = int(numeros)
+                        ultimosNumerosCPF.append(numeros)
+            else:
+                raise ValueError
+
+        except:
+            os.system('cls')
+            print(f'{vermelho}erro: CPF False!{branco}')
+            continue
+
+        try:
+            if len(primeiros_numeros_CPF) != 9 and len(ultimosNumerosCPF) == 2:
+                raise ValueError
+        except:
+            os.system('cls')
+            print(f'{vermelho}erro: tamanho do CPF!{branco}')
+            continue
+        
+        cpf = ''
+        for numero in primeiros_numeros_CPF:
+            cpf += str(numero)
+        for numero in ultimosNumerosCPF:
+            cpf += str(numero)
+        cpf = int(cpf)
+    
+    cpf = list(str(cpf))
+    contador = 10
+    total_soma_cpf = 0
+    for indice, numero in enumerate(cpf):
+        if indice > 8:
+            break
+        else:
+            numero = int(numero)
+            total_soma_cpf += numero * contador
+            contador -= 1
+
+    total_soma_cpf *= 10
+    total_soma_cpf = total_soma_cpf % 11
+    
+
+    if total_soma_cpf > 9:
+        total_soma_cpf = 0
+
+    total_soma_cpf_digito2 = 0
+    contador = 11
+    for indice, numero in enumerate(cpf):
+        if indice > 9:
+            break
+        else:
+            numero = int(numero)
+            total_soma_cpf_digito2 += numero * contador
+            contador -= 1
+
+    total_soma_cpf_digito2 *= 10
+    total_soma_cpf_digito2 = total_soma_cpf_digito2 % 11
+
+    if total_soma_cpf_digito2 > 9:
+        total_soma_cpf_digito2 = 0
+    
+    resultado_do_calculo = str(total_soma_cpf) + str(total_soma_cpf_digito2)
+
+    if resultado_do_calculo == ''.join(cpf[-2:]):
+        os.system('cls')
+        print(f'{verde}CPF Valido!{branco}')
+    else:
+        os.system('cls')
+        print(f'{vermelho}CPF Inválido ou nao existente!{branco}')
+
+
+
+
+
+
+
+
+
+
+
+
